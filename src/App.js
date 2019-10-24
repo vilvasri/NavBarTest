fb5d27cabdee8a6e221aa5a11d84c54a5acfa253
Vilvasri Subramanian <svilvasri@athenahealth.com>
2019-10-24 14:49:54 +0530

    Adding responsive nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'rsuite/dist/styles/rsuite-default.css';
+import ResponsiveNav from '@rsuite/responsive-nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+    { eventKey: 'A', label: 'Item A' },
+    { eventKey: 'B', label: 'Item B' },
+    { eventKey: 'C', label: 'Item C' },
+    { eventKey: 'D', label: 'Item D' },
+    { eventKey: 'E', label: 'Item E' },
+    { eventKey: 'F', label: 'Item F' },
+    { eventKey: 'G', label: 'Item G' },
+    { eventKey: 'H', label: 'Item H' },
+    { eventKey: 'I', label: 'Item I' },
+    { eventKey: 'J', label: 'Item J' },
+    { eventKey: 'K', label: 'Item K' },
+    { eventKey: 'L', label: 'Item L' },
+    { eventKey: 'M', label: 'Item M' },
+    { eventKey: 'N', label: 'Item N' }
+];
 
+const App = () => {
+    const [activeKey, setActiveKey] = React.useState('A');
+
+    return (
+        <div>
+            <div style={{border: '1px solid #ddd', padding: 10 }}>
+                <ResponsiveNav
+                    activeKey={activeKey}
+                    onSelect={setActiveKey}
+                    appearance="subtle" justified
+                >
+                    {items.map(item => (
+                        <ResponsiveNav.Item key={item.eventKey} eventKey={item.eventKey}>
+                            {item.label}
+                        </ResponsiveNav.Item>
+                    ))}
+                </ResponsiveNav>
+            </div>
+        </div>
+    );
+};
 export default App;
